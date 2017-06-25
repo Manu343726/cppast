e360c260d0eb7fa2e4aeeeecf651f7b2cddca010
Manu343726 <Manu343726@gmail.com>
2017-06-25 17:59:51 +0200

    (matchers) variadic matchers
    This commit introduces variadic_matcher, a type-erased matcher
    implementing different boolean operations to evaluate the result of
    n inner matchers. This inner matchers evaluation operators are the
    basis for more complex matcher expression that rrquire the evaluation of
    different sub-matchers.

---
+++ include/cppast/matchers/variadic_matcher.hpp
@@ -0,0 +1,80 @@
+// Copyright (C) 2017 Manu Sanchez <Manu343726@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#ifndef CPPAST_MATCHERS_VARIADIC_MATCHER_INCLUDED
+#define CPPAST_MATCHERS_VARIADIC_MATCHER_INCLUDED
+
+#include <cppast/matchers/matcher_interface.hpp>
+#include <vector>
+
+namespace cppast
+{
+
+namespace matchers
+{
+
+class variadic_matcher : public basic_matcher_interface
+{
+public:
+    enum class variadic_operator
+    {
+        all_of,
+        any_of,
+        each_of,
+        none_of
+    };
+
+    variadic_matcher(variadic_operator op, std::vector<type_erased_matcher> matchers);
+
+    template<typename... Matchers>
+    variadic_matcher(const variadic_operator op, Matchers&&... inner_matchers) :
+        variadic_matcher{
+            op,
+            std::vector<type_erased_matcher>{
+                type_erased_matcher{std::forward<Matchers>(inner_matchers)}...
+            }
+        }
+    {}
+
+    bool matches(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder) const override final;
+
+private:
+    type_erased_matcher _matcher;
+};
+
+template<typename... Matchers>
+variadic_matcher all_of(Matchers&&... inner_matchers)
+{
+    return {variadic_matcher::variadic_operator::all_of, std::forward<Matchers>(inner_matchers)...};
+}
+
+template<typename... Matchers>
+variadic_matcher any_of(Matchers&&... inner_matchers)
+{
+    return {variadic_matcher::variadic_operator::any_of, std::forward<Matchers>(inner_matchers)...};
+}
+
+template<typename... Matchers>
+variadic_matcher each_of(Matchers&&... inner_matchers)
+{
+    return {variadic_matcher::variadic_operator::each_of, std::forward<Matchers>(inner_matchers)...};
+}
+
+template<typename... Matchers>
+variadic_matcher none_of(Matchers&&... inner_matchers)
+{
+    return {variadic_matcher::variadic_operator::none_of, std::forward<Matchers>(inner_matchers)...};
+}
+
+template<typename Matcher>
+variadic_matcher unless(Matcher&& inner_matcher)
+{
+    return none_of(std::forward<Matcher>(inner_matcher));
+}
+
+}
+
+}
+
+#endif // CPPAST_MATCHERS_VARIADIC_MATCHER_INCLUDED
---
+++ src/matchers/variadic_matcher.cpp
@@ -0,0 +1,112 @@
+// Copyright (C) 2017 Manu Sanchez <Manu343726@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#include <cppast/matchers/variadic_matcher.hpp>
+#include <numeric>
+
+using namespace cppast;
+using namespace cppast::matchers;
+
+namespace variadic_functions
+{
+
+bool all_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder, const std::vector<type_erased_matcher>& inner_matchers);
+bool any_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder, const std::vector<type_erased_matcher>& inner_matchers);
+bool each_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder, const std::vector<type_erased_matcher>& inner_matchers);
+bool none_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder, const std::vector<type_erased_matcher>& inner_matchers);
+
+}
+
+using variadic_matcher_function = bool(*)(const cpp_entity&, ast_match_finder&, bound_nodes_tree_builder&, const std::vector<type_erased_matcher>&);
+
+template<variadic_matcher_function Func>
+class variadic_matcher_impl : public basic_matcher_interface
+{
+public:
+    bool matches(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder) const override final
+    {
+        return Func(node, finder, builder, _inner_matchers);
+    }
+
+    static type_erased_matcher make(std::vector<type_erased_matcher> inner_matchers)
+    {
+        return {variadic_matcher_impl{std::move(inner_matchers)}};
+    }
+
+private:
+    std::vector<type_erased_matcher> _inner_matchers;
+
+    variadic_matcher_impl(std::vector<type_erased_matcher> inner_matchers) :
+        _inner_matchers(std::move(inner_matchers))
+    {}
+};
+
+type_erased_matcher variadic_matcher_impl_for(variadic_matcher::variadic_operator op, std::vector<type_erased_matcher> inner_matchers)
+{
+    switch(op)
+    {
+    case variadic_matcher::variadic_operator::all_of:
+        return variadic_matcher_impl<variadic_functions::all_of>::make(std::move(inner_matchers));
+    case variadic_matcher::variadic_operator::any_of:
+        return variadic_matcher_impl<variadic_functions::any_of>::make(std::move(inner_matchers));
+    case variadic_matcher::variadic_operator::each_of:
+        return variadic_matcher_impl<variadic_functions::each_of>::make(std::move(inner_matchers));
+    case variadic_matcher::variadic_operator::none_of:
+        return variadic_matcher_impl<variadic_functions::none_of>::make(std::move(inner_matchers));
+    }
+}
+
+variadic_matcher::variadic_matcher(variadic_matcher::variadic_operator op, std::vector<type_erased_matcher> inner_matchers) :
+    _matcher{variadic_matcher_impl_for(op, std::move(inner_matchers))}
+{}
+
+bool variadic_matcher::matches(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder) const
+{
+    return _matcher.matches(node, finder, builder);
+}
+
+namespace variadic_functions
+{
+
+bool all_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder, const std::vector<type_erased_matcher>& inner_matchers)
+{
+    return std::all_of(inner_matchers.begin(), inner_matchers.end(), [&node, &finder, &builder](const type_erased_matcher& matcher)
+    {
+        return matcher.matches(node, finder, builder);
+    });
+}
+
+bool any_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder, const std::vector<type_erased_matcher>& inner_matchers)
+{
+    return std::any_of(inner_matchers.begin(), inner_matchers.end(), [&node, &finder, &builder](const type_erased_matcher& matcher)
+    {
+        return matcher.matches(node, finder, builder);
+    });
+}
+
+bool each_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder& builder, const std::vector<type_erased_matcher>& inner_matchers)
+{
+    bool result = false;
+
+    for(const auto& matcher : inner_matchers)
+    {
+        // No short-circuiting, all matchers are evaluated
+        result |= matcher.matches(node, finder, builder);
+    }
+
+    return result;
+}
+
+bool none_of(const cpp_entity& node, ast_match_finder& finder, bound_nodes_tree_builder&, const std::vector<type_erased_matcher>& inner_matchers)
+{
+    return std::none_of(inner_matchers.begin(), inner_matchers.end(), [&node, &finder](const type_erased_matcher& matcher)
+    {
+        // Ignore nodes bound by matching inner matchers,
+        // since the result is negative
+        bound_nodes_tree_builder dummy_builder;
+        return matcher.matches(node, finder, dummy_builder);
+    });
+}
+
+}
